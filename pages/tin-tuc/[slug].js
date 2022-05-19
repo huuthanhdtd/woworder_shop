903a213f36b7af8106c444fa0bcafa94c964ed74
quanghuyTipici <quanghuy.tipici@gmail.com>
2022-05-19 16:34:10 +0700

    edit detail page tin tuc

--- pages/tin-tuc/[slug].js
+++ pages/tin-tuc/[slug].js
@@ -2,75 +2,77 @@ import { fetchAPI } from '../../lib/api';
 import Seo from '../../components/seo';
 import DetailArticle from '../../components/News/Detail';
 import { reverse } from '../../lib/reverse';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const Article = ({ article, articles }) => {
-  const seo = {
-    metaTitle: article.attributes.title,
-    metaDescription: article.attributes.description,
-    shareImage: article.attributes.image,
-    article: true,
-  };
+const Article = () => {
+  const { query } = useRouter();
+  const [resp, setGetData] = useState({
+    allArticles: null,
+    article: null,
+    seo: null,
+  });
+
+  useEffect(() => {
+    const filter = {
+      category: 'video',
+      slug: query.slug,
+    };
+
+    const fetchData = async () => {
+      const [articleRes, allArticlesRes] = await Promise.all([
+        fetchAPI('/news-articles', {
+          filters: {
+            slug: query.slug,
+          },
+          populate: '*',
+        }),
+        fetchAPI('/news-articles', {
+          populate: '*',
+        }),
+      ]);
+
+      setGetData({
+        allArticles: reverse(
+          allArticlesRes.data.filter((article) => {
+            for (let key in filter) {
+              if (
+                article.attributes[key] === undefined ||
+                article.attributes[key] === filter[key]
+              )
+                return false;
+            }
+            return true;
+          })
+        ),
+        article: articleRes.data[0],
+        seo: {
+          metaTitle: articleRes.data[0].attributes.title,
+          metaDescription: articleRes.data[0].attributes.description,
+          shareImage: articleRes.data[0].attributes.image,
+          article: true,
+        },
+      });
+    };
+
+    fetchData();
+  }, [query]);
 
   return (
     <>
-      <Seo seo={seo} />
-      <DetailArticle
-        article={article}
-        articleMarkdown={article.attributes.content}
-        anotherArticle={articles}
-        image={article.attributes.image}
-      />
+      {resp.article && (
+        <>
+          <Seo seo={resp.seo} />
+          <DetailArticle
+            article={resp.article}
+            articleMarkdown={resp.article.attributes.content}
+            anotherArticle={resp.allArticles}
+            image={resp.article.attributes.image}
+          />
+        </>
+      )}
     </>
   );
 };
-
-export async function getStaticPaths() {
-  const articlesRes = await fetchAPI('/news-articles', { fields: ['slug'] });
-
-  return {
-    paths: articlesRes.data.map((article) => ({
-      params: {
-        slug: article.attributes.slug,
-      },
-    })),
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const articlesRes = await fetchAPI('/news-articles', {
-    filters: {
-      slug: params.slug,
-    },
-    populate: '*',
-  });
-  const allArticles = await fetchAPI('/news-articles', {
-    populate: '*',
-  });
-
-  const filter = {
-    category: 'video',
-    slug: params.slug,
-  };
-
-  return {
-    props: {
-      article: articlesRes.data[0],
-      articles: reverse(
-        allArticles.data.filter((article) => {
-          for (let key in filter) {
-            if (
-              article.attributes[key] === undefined ||
-              article.attributes[key] === filter[key]
-            )
-              return false;
-          }
-          return true;
-        })
-      ),
-    },
-    revalidate: 1,
-  };
-}
 
 export default Article;
