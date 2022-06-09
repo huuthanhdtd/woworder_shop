bb420cfd5b7c50bae1362eba16533a36f1ff4edb
shanks-tipici <shanks.tipici@gmail.com>
2022-06-09 11:52:42 +0700

    fix speed load website

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@
 const nextConfig = {
   images: {
     loader: 'default',
-    // domains: ['localhost', '86c5-14-250-231-16.ngrok.io'],
+    formats: ['image/avif', 'image/webp'],
     domains: ['localhost', '206.189.83.92'],
   },
 };
-
 module.exports = nextConfig;
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({});
