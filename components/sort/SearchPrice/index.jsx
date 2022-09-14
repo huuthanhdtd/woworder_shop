82edb85c4adff5d215a8c3a61753498e133a1546
minhquyn1204 <68966267+minhquyn1204@users.noreply.github.com>
2022-09-14 19:05:48 +0700

    edit searchPrice show

--- components/sort/SearchPrice/index.jsx
+++ components/sort/SearchPrice/index.jsx
@@ -1,13 +1,17 @@
 import { Button } from '@material-ui/core';
 import clsx from 'clsx';
 import React, { useEffect, useState } from 'react';
-// import { currencyMask } from '../../../lib';
 import styles from './styles.module.scss';
 
 export default function SearchPrice() {
   const [formPrice, setFormPrice] = useState({ priceFirst: '', priceLast: '' });
   const [msg, setmsg] = useState('');
   const handleOnChange = (e) => {
+    e.target.value =
+      !!e.target.value && Math.abs(e.target.value) >= 0
+        ? Math.abs(e.target.value)
+        : null;
+    if (e.target.value.length == 10) return false;
     let { name, value } = e.target;
     value = Number(value);
     setFormPrice({ ...formPrice, [name]: value });
@@ -30,7 +34,11 @@ export default function SearchPrice() {
         <div className={styles.input}>
           <input
             type="number"
-            style={{ width: '47%', padding: '5px' }}
+            style={{
+              width: '47%',
+              padding: '5px',
+              fontSize: '0.75rem',
+            }}
             name="priceFirst"
             placeholder="Từ"
             value={formPrice.priceFirst || ''}
@@ -39,7 +47,11 @@ export default function SearchPrice() {
           <div>-</div>
           <input
             type="number"
-            style={{ width: '47%', padding: '5px' }}
+            style={{
+              width: '47%',
+              padding: '5px',
+              fontSize: '0.75rem',
+            }}
             name="priceLast"
             placeholder="Đến"
             value={formPrice.priceLast || ''}
--- components/sort/checkWeb/index.jsx
+++ components/sort/checkWeb/index.jsx
@@ -1,7 +1,8 @@
-import { Checkbox, FormControlLabel } from '@material-ui/core';
-import React from 'react';
+import { Button, Checkbox, FormControlLabel } from '@material-ui/core';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import styles from './styles.module.scss';
+import { BiChevronsDown, BiChevronsUp } from 'react-icons/bi';
 
 export default function CheckWed({}) {
   const checks = [
@@ -11,24 +12,30 @@ export default function CheckWed({}) {
     { name: 'ASOS', slug: 'ASOS' },
     { name: 'CONVERSE', slug: 'CONVERSE' },
     { name: 'HM', slug: 'HM' },
-    { name: 'IKEA', slug: 'IKEA' },
+    { name: 'PUMA', slug: 'puma' },
+    { name: 'ZUNE', slug: 'zune' },
+    { name: 'BALENCIAGA', slug: 'balenciaga' },
+    { name: 'NIKE', slug: 'nike' },
+    { name: 'GUCCI', slug: 'gucci' },
   ];
   const [checked, setChecked] = useState([]);
-  // const handleChange1 = (isChecked) => {
-  //   if (isChecked) return setChecked(checks.map((data) => data.slug));
-  //   else setChecked([]);
-  // };
-
+  const [loadMore, setLoadMore] = useState(7);
   const handleChangeTwo = (isChecked, slug) => {
     const index = checked.indexOf(slug);
-
     if (isChecked) return setChecked((state) => [...state, slug]);
-
     if (!isChecked && index > -1)
       return setChecked((state) => {
         state.splice(index, 1);
         return JSON.parse(JSON.stringify(state));
       });
+  };
+  useEffect(() => {}, [loadMore]);
+  const handleMore = () => {
+    if (loadMore === 7) {
+      setLoadMore(checks.length);
+    } else {
+      setLoadMore(7);
+    }
   };
 
   return (
@@ -36,7 +43,7 @@ export default function CheckWed({}) {
       <h2> Quốc gia</h2>
       <div className={styles.FormControl}>
         {checked &&
-          checks.map((data, index) => (
+          checks.slice(0, loadMore).map((data, index) => (
             <div key={index} className={styles.checked}>
               <label className={styles.label}>
                 <input
@@ -50,7 +57,22 @@ export default function CheckWed({}) {
               </label>
             </div>
           ))}
-      </div>{' '}
+        {checks.length > 7 ? (
+          <div className={styles.loadMore} onClick={handleMore}>
+            {loadMore === 7 ? (
+              <>
+                xem thêm <BiChevronsDown />
+              </>
+            ) : (
+              <>
+                ẩn bớt <BiChevronsUp />
+              </>
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
